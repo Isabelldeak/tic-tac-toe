7791550eccd3e56f866ad46c578ee07e9eefa127
Isabelldeak <54918648+Isabelldeak@users.noreply.github.com>
2022-02-11 18:07:24 -0700

    implemented displaybranch

---
+++ TicTacToe.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string** CreateBoard(){
+    string ** board = new string*[3];
+    for (int i = 0; i < 3; ++i){
+      board[i] = new string[3];
+      for (int j = 0; j< 3; ++j){
+        board[i][j] = "-";
+      }
+    }
+
+    return board;
+}
+
+void DisplayBoard(string **board){
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      cout << board[i][j];
+    }
+    cout << endl;
+
+
+  }
+
+}
+
+
+int main(){
+
+  string** board = CreateBoard();
+
+  DisplayBoard(board);
+
+}
