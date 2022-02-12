768e7d2be9c182f13328b4183993e29c714ec5b6
Isabelldeak <54918648+Isabelldeak@users.noreply.github.com>
2022-02-11 18:05:29 -0700

    added displayboard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,11 +3,35 @@
 using namespace std;
 
 
-void CreateBoard(){
-  string board[3][3];
+string** CreateBoard(){
+    string ** board = new string*[3];
+    for (int i = 0; i < 3; ++i){
+      board[i] = new string[3];
+      for (int j = 0; j< 3; ++j){
+        board[i][j] = "-";
+      }
+    }
+
+    return board;
+}
+
+void DisplayBoard(string **board){
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      cout << board[i][j];
+    }
+    cout << endl;
+
+
+  }
+
 }
 
 
 int main(){
-  CreateBoard();
+
+  string** board = CreateBoard();
+
+  DisplayBoard(board);
+
 }
